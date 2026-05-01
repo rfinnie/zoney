6e37f4c5536eeb3007023c45b90e98ed13109973
Ryan Finnie <ryan@finnie.org>
2026-05-01 10:46:23 -0700

    Bump requires-python; add explicit build-system; add stub setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# SPDX-PackageName: zoney
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://codeberg.org/rfinnie/zoney
+# SPDX-FileCopyrightText: © 2026 Ryan Finnie <ryan@finnie.org>
+# SPDX-License-Identifier: MPL-2.0
+
+from setuptools import setup
+
+setup()
